bc1ba180cefb1f18c5fa94aee8a28b4d78e4f56b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-12-14 16:36:03 -0500

    Link Button color

--- client/src/components/ui/LinkButton.jsx
+++ client/src/components/ui/LinkButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LinkButton(props) {
   return (
-    <button className="text-black/70 text-sm font-normal">
+    <button className="text-black/70 text-sm font-normal hover:text-black">
       {props.children}
     </button>
   );
